d38f828fe6111fb391a3633288f0c3a6e53d79b3
H3h3QAQ <63571676+H3h3QAQ@users.noreply.github.com>
2022-09-01 19:28:43 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'H3h3QAQ\'s blog',
+  author: 'h3h3qaq',
+  email: 'h3h3qaq@qq.com',
   link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  description: 'A place to think and write.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
